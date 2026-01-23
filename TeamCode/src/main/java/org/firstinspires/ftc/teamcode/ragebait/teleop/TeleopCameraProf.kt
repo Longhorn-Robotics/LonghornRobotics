e6be02983ebdbcb29f47557f9c159755619ab78e
Anticubex <teofontana00@gmail.com>
2026-01-22 16:05:24 -0800

    Did profiling on camera and bulk reads stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ragebait/teleop/TeleopCameraProf.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ragebait/teleop/TeleopCameraProf.kt
@@ -1,22 +1,36 @@
 package org.firstinspires.ftc.teamcode.ragebait.teleop
 
 import com.pedropathing.geometry.Pose
+import com.qualcomm.hardware.lynx.LynxModule
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorEx
 import com.qualcomm.robotcore.util.ElapsedTime
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName
 import org.firstinspires.ftc.teamcode.ragebait.auton.GetPoseFromCamera
-import org.firstinspires.ftc.teamcode.ragebait.hardware.RobotHardwareLite
-import org.firstinspires.ftc.teamcode.ragebait.utils.ButtonAction
-import kotlin.math.max
-import kotlin.math.min
 import kotlin.math.sqrt
+
+
+// Pure camera: 0.0016, dev = 0.0063
+// Encoder with nothing else: no change
+// Reading encoder once: 0.0035 | 0.0137
+// Reading encoder twice: 0.005 | 0.0125
+// Reading encoder thrice: 0.008 | 0.0235
+// Bulk reads (1): 0.004 | 0.009
+// Array bulk reads: 0.004
+// Hardcoded hub: 0.004 | 0.0011
+// Future tests are hardcoded hub
+// Bulk reads (2): 0.0042 | 0.0012
+// Bulk reads (3): 0.0043 | 0.0035
+//
+// BIG MONEY
+
 
 @TeleOp(name = "TeleopCameraProf", group = "Testing")
 class TeleopCameraProf : OpMode() {
-    var robot: RobotHardwareLite = RobotHardwareLite()
-    var testMotor1Speed: Double = 0.0
-    var testMotor2Speed: Double = 0.0
 
+    lateinit var encoderTest : DcMotorEx
     var camPose : Pose? = null
 
     var currentTime : Double = 0.0
@@ -29,24 +43,14 @@ class TeleopCameraProf : OpMode() {
     private val loopElapsedTime = ElapsedTime()
     private val lastCameraElapsedTime = ElapsedTime()
 
-
-    private val buttonActions = arrayOf(
-        //Increment Motor 2
-        ButtonAction({ gamepad1.dpad_right || gamepad2.dpad_right }, {
-            testMotor2Speed += 0.05
-            testMotor2Speed = min(testMotor2Speed, 1.0)
-            testMotor2Speed = max(testMotor2Speed, -1.0)
-        }),
-        ButtonAction({ gamepad1.dpad_left || gamepad2.dpad_left }, {
-            testMotor2Speed -= 0.05
-            testMotor2Speed = min(testMotor2Speed, 1.0)
-            testMotor2Speed = max(testMotor2Speed, -1.0)
-        }),
-    )
-
+    val hub: LynxModule by lazy { hardwareMap.getAll(LynxModule::class.java)[0] }
     // Code to run once when the driver hits INIT
     override fun init() {
-        robot.init(hardwareMap)
+        GetPoseFromCamera.initAprilTag(hardwareMap.get(WebcamName::class.java, "Webcam 1"), telemetry)
+        encoderTest = hardwareMap.get(DcMotorEx::class.java, "motor1")
+        encoderTest.power = 0.0
+        encoderTest.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.FLOAT
+        hub.bulkCachingMode = LynxModule.BulkCachingMode.MANUAL
     }
 
     // Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
@@ -57,11 +61,17 @@ class TeleopCameraProf : OpMode() {
     override fun start() {
         loopElapsedTime.reset()
         lastCameraElapsedTime.reset()
-        GetPoseFromCamera.initAprilTag(robot, telemetry)
     }
 
     // Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
     override fun loop() {
+
+        hub.clearBulkCache()
+
+        telemetry.addData("Encoder value 1: ", encoderTest.currentPosition)
+        telemetry.addData("Encoder value 2: ", encoderTest.currentPosition)
+        telemetry.addData("Encoder value 3: ", encoderTest.currentPosition)
+
         //Getting Cam
         camPose = GetPoseFromCamera.getPose("pedropathing")
 
