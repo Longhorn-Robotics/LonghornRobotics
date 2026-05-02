044df99145aa1501d87681f1debbc39370d188d6
Anticubex <teofontana00@gmail.com>
2026-05-02 12:08:00 -0700

    Added documentation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ragebait/systems/core/ErrExtenders.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ragebait/systems/core/ErrExtenders.kt
@@ -1,7 +1,32 @@
 package org.firstinspires.ftc.teamcode.ragebait.systems.core
 
 /**
- * Actually call with just a dot `nullable.withErr(...)` or it will silently skip the error
+ * A set of extension functions to work with errors within the Subsystem error system. They
+ * alleviate the need for explicit catching blocks, while also making the possibility of error
+ * more explicit. They handle logging the error automatically.
+ *
+ * Some use Null as an error-by-value, very akin to Rust's Option/Result methods `map`, `or_err`, etc.
+ * Some use actual kotlin/jvm exceptions
+ *
+ * Further improvements:
+ * Could we replace the ever-present `err: string` term with something different? For example, a closure
+ * to make it so code doesn't always have to generate the error value, or maybe put such extensions
+ * in their own separate overloads.
+ * We might replace the usage of null with an actual `Result`-like type, maybe also as just new
+ * methods rather than replacing the existing Null-y methods. This also leads naturally into adding
+ * a proper error type, instead of the current system of just using string.
+ * */
+
+/**
+ * Execute some code conditional on a value being nonnull.
+ *
+ * If the value is null, it logs a Subsystem error, otherwise it executes the block.
+ * Warning: Don't call this with a `.?` null-safe operator, as that will silently skip this.
+ *
+ * @param err The error message to send if the value is null.
+ * @param block The closure to execute on the value if it isn't null.
+ * @return Null if the original value was null, otherwise the return value of the block (which may
+ *  itself be null)
  * */
 inline fun <T : Any, R> T?.withErr(err: String, block: (T) -> R): R? = if (this != null) {
     block(this)
@@ -11,7 +36,11 @@ inline fun <T : Any, R> T?.withErr(err: String, block: (T) -> R): R? = if (this 
 }
 
 /**
- * Actually call with just a dot `nullable.orErr(...)` or it will silently skip the error
+ * Logs an error on a null value, and passes execution along.
+ *
+ * Warning: Don't call this with a `.?` null-safe operator, as that will silently skip this.
+ * @param err The error message to send if the value is null.
+ * @return The original value, even if it is null.
  * */
 fun <T : Any> T?.orErr(err: String): T? = if (this != null) this else {
     SubSystem.pushError(err)
@@ -19,12 +48,23 @@ fun <T : Any> T?.orErr(err: String): T? = if (this != null) this else {
 }
 
 /**
- * Runs some code, catching exceptions and turning them into logged errors.
- * It adds some context to the error with the exception.
+ * Runs some code, catching exceptions and logging them as subsystem errors. Adds some context to
+ * the error with the exception.
+ *
  * This *only* catches exceptions, not null values.
+ *
  * If you want to also do special exception handling, you can leverage that this
  * returns null on error and do some sort of `?: run` and/or consider using a side-effecting
  * lambda block to isolate errors.
+ * For example, if I wanted to log the error but then recover with another value, I might do this:
+ * ```
+ * val input = tryErr("Failed to process input, using defaults") { getInputs() }
+ *             ?: { defaultInputs }
+ * ```
+ *
+ * @param err The error message to add context to the exception when logging.
+ * @param block The block to run within the exception catching block.
+ * @return Null if an exception occurred, otherwise the return value of the block.
  */
 inline fun <T, R> T.tryErr(err: String, block: (T) -> R): R? {
     try {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ragebait/systems/core/utils/EnumBucket.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ragebait/systems/core/utils/EnumBucket.kt
@@ -7,12 +7,16 @@ import kotlin.enums.enumEntries
 
 // I LOVE TYPE ERASURE
 // TYPE ERASURE MAKES ME NEED AN ENTIRE HELPER CONSTRUCTOR!
+/**
+ * Helper constructor to create a new exhaustive enum map, with an initial value.
+ * Necessary because of type erasure.
+ */
 inline fun <reified K : Enum<K>, T> exhaustiveEnumMap(noinline init: (K) -> T): ExhaustiveEnumMap<K, T> = ExhaustiveEnumMap(K::class.java, enumEntries<K>(), init)
 
 /**
  * A class that provides an enum map guaranteed to be exhaustive.
  * Allowing use of indexing as purely non-null (unless you chose to
- * make `T` a nullable type for some unexplained reason.
+ * make `T` a nullable type for some unexplained reason.)
  * */
 class ExhaustiveEnumMap<K: Enum<K>, T>(private val kClass: Class<K>, private val entries: EnumEntries<K>, init: (K) -> T) {
     private val enumMap by lazy {
@@ -23,10 +27,23 @@ class ExhaustiveEnumMap<K: Enum<K>, T>(private val kClass: Class<K>, private val
         map
     }
 
+    /**
+     * Gets a value from the map
+     */
     operator fun get(key: K): T = enumMap.getValue(key)
+
+    /**
+     * Sets a value in the map
+     */
     operator fun set(key: K, value: T) {enumMap[key] = value}
 }
 
+/**
+ * Type Alias for the common use case of an Enum map into a set of values, i.e. using enum "buckets"
+ */
 typealias EnumBucket<K, T> = ExhaustiveEnumMap<K, MutableSet<T>>
-/** Necessary helper constructor because of type erasure */
+/**
+ * Helper constructor to create a new fully empty enum bucket.
+ * Necessary because of type erasure.
+ */
 inline fun <reified K : Enum<K>, T> enumBucket(): EnumBucket<K, T> = exhaustiveEnumMap { mutableSetOf() }
